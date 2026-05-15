feb55800ace32d1a0c2a87dc2b798c100cdea297
JudgeXiong1 <78939340+JudgeXiong1@users.noreply.github.com>
2026-05-15 03:15:49 -0500

    Update prim.h

--- prim.h
+++ prim.h
@@ -1,1 +1,13 @@
 //Please put header information in this file.
+#ifndef PRIM_H
+#define PRIM_H
+
+#include <vector>
+
+struct Edge {
+    int to;
+    int weight;
+};
+
+// This tells the compiler that primMST exists elsewhere
+void primMST(int n, const std::vector<std::vector<Edge>>& adj);
